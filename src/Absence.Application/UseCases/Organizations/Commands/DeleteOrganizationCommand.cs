a06ed9442af3963464b33b91ee90148fd2f0f242
Ihor Reva <igoryan.reva.01@icloud.com>
2026-08-20 21:11:44 +0200

    fixed: incorrect results returned from delete organization endpoint

--- src/Absence.Application/UseCases/Organizations/Commands/DeleteOrganizationCommand.cs
+++ src/Absence.Application/UseCases/Organizations/Commands/DeleteOrganizationCommand.cs
@@ -6,7 +6,7 @@ using OneOf.Types;
 
 namespace Absence.Application.UseCases.Organizations.Commands;
 
-public class DeleteOrganizationCommand(int id, DTOs.DeleteOrganizationRequest request) : IRequest<OneOf<Success, AccessDenied, NotFound>>
+public class DeleteOrganizationCommand(int id, DTOs.DeleteOrganizationRequest request) : IRequest<OneOf<Success, NotFound, AccessDenied>>
 {
     public int Id { get; } = id;
     public DeleteOrganizationRequest Request { get; } = request;
